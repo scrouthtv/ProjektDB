bc478faa483289f525260cf3908189df2ceb8e87
Lenni <lennivh24@gmail.com>
2020-06-17 07:53:26 +0000

    Version of 16.06. 22:12 Version 1.0 of Frontend main page

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+
 function checkInput() {
-    alert(":)");
+    if(document.getElementById("who").value == "" ||
+    document.getElementById("room").value == "" ||
+    document.getElementById("what").value == "" ||
+    document.getElementById("why").value == "") {
+        document.getElementById("submit").disabled=true;
+        document.getElementById("submit").innerHTML="✗";
+    } else {
+        document.getElementById("submit").disabled=false;
+        document.getElementById("submit").innerHTML="✓";
+    }
 }
