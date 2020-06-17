592374bfe8614c4aab92aed6f659442a9caa63c5
Lenni vH <lennivh24@gmail.com>
2020-06-17 12:38:22 +0200

    pushing template

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-
 function checkInput() {
-    if(document.getElementById("who").value == "" ||
-    document.getElementById("room").value == "" ||
-    document.getElementById("what").value == "" ||
-    document.getElementById("why").value == "") {
-        document.getElementById("submit").disabled=true;
-        document.getElementById("submit").innerHTML="✗";
-    } else {
-        document.getElementById("submit").disabled=false;
-        document.getElementById("submit").innerHTML="✓";
-    }
+	if(document.getElementById("who").value == "" ||
+		document.getElementById("room").value == "" ||
+		document.getElementById("what").value == "" ||
+		document.getElementById("why").value == "") {
+		document.getElementById("submit").disabled=true;
+		document.getElementById("submit").innerHTML="✗";
+	} else {
+		document.getElementById("submit").disabled=false;
+		document.getElementById("submit").innerHTML="✓";
+	}
+}
+
+function push() {
+	var xhtpp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			alert(xhttp.responseText);
+		}
+	};
+	xhttp.open("GET", "push.php", true);
+	xhttp.send();
 }
