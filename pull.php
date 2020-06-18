3641bd8d46454940b6b7bff0810c461e71cd6f58
Lenni vH <lennivh24@gmail.com>
2020-06-18 21:32:23 +0200

    deleting also works

--- pull.php
+++ pull.php
@@ -1,20 +1,20 @@
 <?php
 
 echo "[";
-#echo '{"id": 5, "who": "me", "room": 27, "what": "Smartboard", "why": "Schaltet nicht ein"},';
-echo '{"id": 12, "who": "you", "room": 31, "what": "Beamer", "why": "flackert"},';
-echo '{"id": 11, "who": "you", "room": 31, "what": "Beamer", "why": "flackert"},';
-echo '{"id": 13, "who": "you", "room": 31, "what": "Beamer", "why": "flackert"},';
-echo '{"id": 14, "who": "you", "room": 31, "what": "Beamer", "why": "flackert"},';
-echo '{"id": 15, "who": "you", "room": 31, "what": "Beamer", "why": "flackert"},';
-echo '{"id": 17, "who": "you", "room": 31, "what": "Beamer", "why": "flackert"},';
-echo '{"id": 18, "who": "you", "room": 31, "what": "Beamer", "why": "flackert"},';
-echo '{"id": 25, "who": "you", "room": 31, "what": "Beamer", "why": "flackert"},';
-echo '{"id": 32, "who": "you", "room": 31, "what": "Beamer", "why": "flackert"},';
-echo '{"id": 22, "who": "you", "room": 31, "what": "Beamer", "why": "flackert"},';
-echo '{"id": 23, "who": "you", "room": 31, "what": "Beamer", "why": "flackert"},';
-echo '{"id": 24, "who": "you", "room": 31, "what": "Beamer", "why": "flackert"},';
-echo '{"id": 31, "who": "sne", "room": 15, "what": "Apple TV", "why": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}';
+echo '{"id": 5, "who": "me", "room": 27, "what": "Smartboard", "why": "Schaltet nicht ein"}';
+#echo '{"id": 12, "who": "you", "room": 31, "what": "Beamer", "why": "flackert"},';
+#echo '{"id": 11, "who": "you", "room": 31, "what": "Beamer", "why": "flackert"},';
+#echo '{"id": 13, "who": "you", "room": 31, "what": "Beamer", "why": "flackert"},';
+#echo '{"id": 14, "who": "you", "room": 31, "what": "Beamer", "why": "flackert"},';
+#echo '{"id": 15, "who": "you", "room": 31, "what": "Beamer", "why": "flackert"},';
+#echo '{"id": 17, "who": "you", "room": 31, "what": "Beamer", "why": "flackert"},';
+#echo '{"id": 18, "who": "you", "room": 31, "what": "Beamer", "why": "flackert"},';
+#echo '{"id": 25, "who": "you", "room": 31, "what": "Beamer", "why": "flackert"},';
+#echo '{"id": 32, "who": "you", "room": 31, "what": "Beamer", "why": "flackert"},';
+#echo '{"id": 22, "who": "you", "room": 31, "what": "Beamer", "why": "flackert"},';
+#echo '{"id": 23, "who": "you", "room": 31, "what": "Beamer", "why": "flackert"},';
+#echo '{"id": 24, "who": "you", "room": 31, "what": "Beamer", "why": "flackert"},';
+#echo '{"id": 31, "who": "sne", "room": 15, "what": "Apple TV", "why": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}';
 echo "]";
 
 ?>
